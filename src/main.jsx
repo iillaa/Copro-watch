d18edb73c668e6bd37c6bfaf8e49b0fd0f2a696e
iillaa <dr.doodo257@gmail.com>
2026-01-31 20:16:03 +0100

    added On-Device Inspector Eruda

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import ErrorBoundary from './components/ErrorBoundary'; // Import it
+import ErrorBoundary from './components/ErrorBoundary'; 
 import './index.css';
+
+// [NEW] START: Mobile Inspector
+import eruda from 'eruda';
+eruda.init();
+// [NEW] END
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
-      {' '}
-      {/* Wrap App */}
       <App />
     </ErrorBoundary>
   </React.StrictMode>
